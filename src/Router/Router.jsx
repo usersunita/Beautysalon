c3a030f9c20d89c8177cb429135c2a08e7cee5c0
Sudip Raut <sudipraut444@gmail.com>
2025-02-02 13:25:37 +0545

    productpagecomplefilter

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -7,6 +7,7 @@ import Register from "../page/Register";
 import Login from "../page/Login";
 import Products from "../page/Products";
 import DetailServices from "../page/DetailServices";
+import Collection from "../page/Collection";
 
 const Router = () => {
   return (
@@ -24,6 +25,8 @@ const Router = () => {
           <Route path="/services/:name" element={<DetailServices />} />
           <Route path="/gallery" element={<Home />} />
           <Route path="/testmonial" element={<Home />} />
+          <Route path="/collection" element={<Collection   />} />
+
           <Route path="/contact" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-import { ShopProvider } from "../src/component/ShopContext";
+import { ShopProvider } from "./context/ShopContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
